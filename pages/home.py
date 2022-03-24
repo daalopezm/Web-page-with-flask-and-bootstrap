c208bbf1bd6493b9ed3440e9e18abb1a8a04edb9
Daniel Alejandro Lopez <danielalejandrol14@gmail.com>
2022-03-23 20:41:26 -0500

    Added gases description

--- pages/home.py
+++ pages/home.py
@@ -1,7 +1,9 @@
+from fileinput import filename
 from flask import Blueprint, render_template, url_for
 import os
 
 home = Blueprint('home', __name__)
+
 
 @home.route("/home")
 @home.route("/")
@@ -10,7 +12,7 @@ def home_page():
               'video1': url_for('static', filename='videos/code.mp4'),
               'video2': url_for('static', filename='videos/blackhole.mp4'),
               'video3': url_for('static', filename='videos/physics.mp4')}
+    videos_programming = {'video_programming_gases': url_for('static', filename='videos/programming/Gases.mp4')}
     images = {f'image{num}': url_for('static', filename='images/{}'.format(
         os.listdir('./static/images/')[num])) for num in range(9)}
-    return render_template('index.html', **images, **videos)
-
+    return render_template('index.html', **images, **videos, **videos_programming)
