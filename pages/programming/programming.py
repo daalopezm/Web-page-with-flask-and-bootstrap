d51376d5dd27d032b39ca0614c9e15c40ac5a7b0
Daniel Alejandro Lopez <danielalejandrol14@gmail.com>
2022-03-24 23:41:14 -0500

    Programming improved

--- pages/programming/programming.py
+++ pages/programming/programming.py
@@ -1,9 +1,13 @@
-from flask import Blueprint, render_template
+from fileinput import filename
+from flask import Blueprint, render_template,url_for
 import os
 
 programming = Blueprint('programming',__name__,static_folder='../.././.././static',template_folder='../.././.././templates')
 
 @programming.route("/")
 def programming_page():
-    return render_template('programming.html')
+    videos_programming = {'video_programming_gases': url_for('static', filename='videos/programming/Gases.mp4')}
+    images_programming = {'image_programming_website': url_for('static', filename='videos/programming/website.png'),
+    'image_programming_gas_explanation_1': url_for('static', filename='videos/programming/gas_explanation/field.png')}
+    return render_template('programming.html',**videos_programming, **images_programming)
 
