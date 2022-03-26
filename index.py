b5af0cd78f889a1958ebd02f948f42c7e0769854
Daniel Alejandro Lopez <danielalejandrol14@gmail.com>
2022-03-26 11:40:38 -0500

    Added blog

--- index.py
+++ index.py
@@ -9,12 +9,14 @@ Author: Daniel Lopez
 from flask import Flask, render_template
 from pages.home import home
 from pages.about import about
+from pages.blog import blog
 from pages.programming.programming import programming
 
 app = Flask(__name__)
 app.register_blueprint(home)
 app.register_blueprint(about, url_prefix='/about')
 app.register_blueprint(programming, url_prefix='/programming')
+app.register_blueprint(blog, url_prefix='/blog')
 
 @app.errorhandler(404)
 def not_found(error):
